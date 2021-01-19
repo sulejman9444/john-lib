521145657db23808c48c45172fd56d151a2dec9f
Sulejman Delic <sulejman.delic@virginpulse.com>
2021-01-19 07:01:37 -0500

    added storybook, render test component task as story

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent {
+  title = '';
+  @Input() task: any;
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onPinTask: EventEmitter<any> = new EventEmitter();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onArchiveTask: EventEmitter<any> = new EventEmitter();
+
+  onArchive(id: any) {
+    this.onArchiveTask.emit(id);
+  }
+}
---
+++ src/stories/Task.stories.ts
@@ -0,0 +1,37 @@
+// src/app/components/task.stories.ts
+import { action } from '@storybook/addon-actions';
+import { TaskComponent } from 'src/app/components/task/task.component';
+
+export default {
+  title: 'Task',
+  excludeStories: /.*Data$/,
+};
+
+export const actionsData = {
+  onArchiveTask: action('onArchiveTask'),
+};
+
+export const taskData = {
+  id: '1',
+  title: 'Test Task',
+  state: 'Task_INBOX',
+  updated_at: new Date(2019, 0, 1, 9, 0),
+};
+export const Default = () => ({
+  component: TaskComponent,
+  props: {
+    task: taskData,
+    onArchiveTask: actionsData.onArchiveTask,
+  },
+});
+// archived task state
+export const Archived = () => ({
+  component: TaskComponent,
+  props: {
+    task: {
+      ...taskData,
+      state: 'TASK_ARCHIVED',
+    },
+    onArchiveTask: actionsData.onArchiveTask,
+  },
+});
